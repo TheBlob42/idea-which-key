9b267f93a0e0e84f1683b539ed48bfa3f48bb64a
TheBlob42 <hessenmobbel@web.de>
2021-01-04 21:20:21 +0100

    Split popup content into columns
    For a better readability and to avoid the height of the Balloon
    becoming to big we split the mappings entries into multiple columns.
    
    Check for the longest entry and use this as a reference to determine
    how many columns can be properly displayed. Then split the mappings
    between those columns.
    
    The width calculation is not perfect yet, but it already makes the
    plugin usage way more enjoyable.

--- src/main/kotlin/eu/theblob42/idea/whichkey/WhichKeyTypeActionHandler.kt
+++ src/main/kotlin/eu/theblob42/idea/whichkey/WhichKeyTypeActionHandler.kt
@@ -4,7 +4,7 @@ import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.actionSystem.ActionPlan
 import com.intellij.openapi.editor.actionSystem.TypedActionHandlerEx
-import com.intellij.openapi.ui.MessageType
+import com.intellij.openapi.editor.colors.EditorColorsManager
 import com.intellij.openapi.ui.popup.Balloon
 import com.intellij.openapi.ui.popup.JBPopupFactory
 import com.intellij.openapi.wm.WindowManager
@@ -12,6 +12,7 @@ import com.intellij.ui.awt.RelativePoint
 import com.maddyhome.idea.vim.VimTypedActionHandler
 import com.maddyhome.idea.vim.command.CommandState
 import com.maddyhome.idea.vim.option.OptionsManager
+import javax.swing.JLabel
 
 class WhichKeyTypeActionHandler(private val vimTypedActionHandler: VimTypedActionHandler): TypedActionHandlerEx {
 
@@ -30,19 +31,44 @@ class WhichKeyTypeActionHandler(private val vimTypedActionHandler: VimTypedActio
         val typedSequence = typedKeysStringBuilder.toString()
             .replace("typed ", "") // remove prefix
 
+        val ideFrame = WindowManager.getInstance().findVisibleFrame()
         val nestedMappings = MappingConfig.getNestedMappings(mappingState.mappingMode, typedSequence)
         // show all available nested mappings in a balloon
-        if (nestedMappings.isNotEmpty()) {
-            val text = nestedMappings.joinToString(separator = "\n")
+        if (ideFrame != null && nestedMappings.isNotEmpty()) {
+            val frameWidth = ideFrame.width
+            val maxString: String = nestedMappings.maxByOrNull { it.length }!! // we have manually checked that nestedMappings is not empty
+            /*
+             * there might be a better way to measure the pixel width of a given String than using a JLabel
+             * so far this method has worked quite well and since I'm missing a better alternative this stays for now
+             */
+            val maxStringWidth = JLabel(maxString).preferredSize.width
+            val possibleColumns = frameWidth / maxStringWidth
+            val elementsPerColumn = Math.ceil(nestedMappings.size / possibleColumns.toDouble()).toInt()
+            val windowedMappings = nestedMappings.windowed(elementsPerColumn, elementsPerColumn, true)
+
+            // to properly align the columns within HTML use a table with fixed with cells
+            val mappingsStringBuilder = StringBuilder()
+            mappingsStringBuilder.append("<table>")
+            for (i in 0..(elementsPerColumn.dec())) {
+                mappingsStringBuilder.append("<tr>")
+                for (column in windowedMappings) {
+                    val entry = column.getOrNull(i)
+                    if (entry != null) {
+                        mappingsStringBuilder.append("<td width=\"$maxStringWidth\">$entry</td>")
+                    }
+                }
+                mappingsStringBuilder.append("</tr>")
+            }
+            mappingsStringBuilder.append("</table>")
+
             val fadeoutTime = if (OptionsManager.timeout.value) {
                 OptionsManager.timeoutlen.value().toLong()
             } else {
                 0L
             }
-            val target = RelativePoint.getSouthWestOf(WindowManager.getInstance().getFrame(editor.project)!!.rootPane)
-
+            val target = RelativePoint.getSouthWestOf(ideFrame.rootPane)
             JBPopupFactory.getInstance()
-                .createHtmlTextBalloonBuilder(text, MessageType.INFO, null)
+                .createHtmlTextBalloonBuilder(mappingsStringBuilder.toString(), null, EditorColorsManager.getInstance().globalScheme.defaultBackground, null)
                 .setFadeoutTime(fadeoutTime)
                 .createBalloon()
                 .show(target, Balloon.Position.above)
