a812bdfab97c9a5f73e163fae99a182fad924d08
Alwyn Schoeman <alwyn.schoeman@pm.me>
2024-01-31 15:55:05 -0500

    Updated gradle build script for the plugin to work on Intellij 2023.3.3 and with IdeaVim 2.8.1
    Signed-off-by: Alwyn Schoeman <alwyn.schoeman@pm.me>

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     java
     kotlin("jvm") version "1.9.22"
-    id("org.jetbrains.intellij") version "1.16.1"
+    id("org.jetbrains.intellij") version "1.17.0"
 }
 
 group = "eu.theblob42.idea.whichkey"
@@ -22,18 +22,18 @@ dependencies {
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "11"
+    kotlinOptions.jvmTarget = "17"
 }
 
 tasks.withType<PatchPluginXmlTask> {
-    sinceBuild.set("232")
+    sinceBuild.set("233")
 }
 
 // See https://github.com/JetBrains/gradle-intellij-plugin/
 intellij {
-    version.set("2023.3.2")
+    version.set("2023.3.3")
     updateSinceUntilBuild.set(false)
-    plugins.set(listOf("IdeaVIM:2.8.0"))
+    plugins.set(listOf("IdeaVIM:2.8.1"))
 }
 
 tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml") {
