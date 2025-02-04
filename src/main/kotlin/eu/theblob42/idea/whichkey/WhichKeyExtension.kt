2a1191a260244decc3091f675adf21c2a8950a20
Lucas Romero <luqasn@gmail.com>
2025-02-04 22:58:54 +0100

    Refactor plugin to use AnActionListener on messageBus
    instead of hooking into ideavim internals.
    Also fixes a bug with gg/zz leaving a dangling popup at the end of the
    sequence.

--- src/main/kotlin/eu/theblob42/idea/whichkey/WhichKeyExtension.kt
+++ src/main/kotlin/eu/theblob42/idea/whichkey/WhichKeyExtension.kt
@@ -1,14 +1,11 @@
 package eu.theblob42.idea.whichkey
 
-import com.intellij.openapi.actionSystem.ActionManager
+import com.intellij.openapi.actionSystem.ex.AnActionListener
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.diagnostic.logger
-import com.intellij.openapi.editor.actionSystem.TypedAction
-import com.maddyhome.idea.vim.VimTypedActionHandler
 import com.maddyhome.idea.vim.extension.VimExtension
-import java.util.*
 
-class WhichKeyExtension: VimExtension {
-
+class WhichKeyExtension : VimExtension {
     companion object {
         val logger = logger<WhichKeyExtension>()
     }
@@ -18,30 +15,9 @@ class WhichKeyExtension: VimExtension {
     override fun init() {
         logger.debug("init IDEA Which-Key extension")
 
-        /*
-         * We have to exchange the 'raw' typing handler which is set by IdeaVim with our own in order to intercept all keystrokes
-         * Unfortunately during the VIM extension init process the IdeaVim handler hasn't been set yet
-         * To ensure the handler is replaced as soon as possible (before the project has loaded completed) we use the following Timer
-         *
-         * Furthermore, we also have to replace the 'VimShortcutKeyAction' of IdeaVim with our own implementation
-         * This is needed to intercept modified (e.g. Ctrl + W) or special key strokes (e.g. Escape) properly
-         */
-        val typedAction = TypedAction.getInstance()
-        val timer = Timer()
-        timer.schedule(object : TimerTask() {
-            override fun run() {
-                // assign to local val for the type check and smart casting to work properly
-                val rawTypedActionHandler = typedAction.rawHandler
-                if (rawTypedActionHandler is VimTypedActionHandler) {
-                    logger.debug("replacing IdeaVim type handler")
-                    typedAction.setupRawHandler(WhichKeyTypeActionHandler(rawTypedActionHandler))
-
-                    logger.debug("replacing IdeaVim 'VimShortcutKeyAction'")
-                    ActionManager.getInstance().replaceAction("VimShortcutKeyAction", WhichKeyShortcutKeyAction())
-                    timer.cancel()
-                }
-            }
-        }, 0L, 500L)
+        ApplicationManager.getApplication()
+            .messageBus.connect(WhichKeyPluginDisposable.instance)
+            .subscribe(AnActionListener.TOPIC, WhichKeyActionListener())
     }
 
 }
